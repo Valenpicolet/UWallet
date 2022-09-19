c2fa09dc4804436c2f1941aef43b986325c37876
martiiih <107011705+martiiih@users.noreply.github.com>
2022-09-19 18:03:39 -0300

    Update FrmRecuperarContrasenia.cs

--- Cripto/Interfaces/Interfaces Clientes/FrmRecuperarContrasenia.cs
+++ Cripto/Interfaces/Interfaces Clientes/FrmRecuperarContrasenia.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PAV
+namespace Cripto.Interfaces.Interfaces_Clientes
 {
     public partial class FrmRecuperarContrasenia : Form
     {
