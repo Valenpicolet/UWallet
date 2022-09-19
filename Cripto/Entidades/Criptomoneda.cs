4482cbbd392e6eecb6084ee8afca5c595a326b6e
Valenpicolet <mariavalentinapicolet@gmail.com>
2022-09-19 17:13:19 -0300

    cambios9

--- Cripto/Entidades/Criptomoneda.cs
+++ Cripto/Entidades/Criptomoneda.cs
@@ -6,6 +6,7 @@
         private string nombre;
         private string abreviacion;
         private int cantidad;
+        private byte[] logo; 
         #endregion
 
         #region Accesores
@@ -15,14 +16,18 @@
         public void setNombre(string nombre) { this.nombre = nombre; } 
         public void setAbreviacion(string abreviacion) { this.abreviacion = abreviacion; }
         public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+        public byte[] Logo { get { return logo; } set { logo = value; } }
         #endregion
 
-        public Criptomoneda(string nombre,string abreviacion) // int cantidad
-        { 
+        public Criptomoneda(string nombre,string abreviacion) // int cantidad, byte[] logo
+        {
             this.nombre = nombre;
             this.abreviacion = abreviacion;
-           // this.cantidad = cantidad;
+            //this.logo = logo;
+            // this.cantidad = cantidad;
         }
 
     }
+  
+
 }
